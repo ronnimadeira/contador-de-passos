ded2608d6ff3c119c6e96099d8d9d9265e921bb6
ronnimadeira <115476262+ronnimadeira@users.noreply.github.com>
2023-03-14 16:53:25 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
 input.onButtonPressed(Button.A, function () {
-    steps = 0
+    passos = 0
     basic.showNumber(0)
 })
-let steps = 0
-steps = 0
+let passos = 0
+passos = 0
 basic.showNumber(0)
 basic.forever(function () {
     if (input.acceleration(Dimension.Strength) > 1500) {
-        steps += 1
-        basic.showNumber(steps)
+        passos += 1
+        basic.showNumber(passos)
     }
 })
